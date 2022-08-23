1c5583803a1ebd6e60b373ee772d4ee354ee8e06
DaboRestrepo <daborestrepo@gmail.com>
2022-08-23 17:25:08 -0500

    make test with enzyme

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,20 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+it('render without crashing', () => {
+  shallow(<App />);
+});
+
+it('render App-header', () => {
+  expect(wrapper.find('header.App-header').exists()).toEqual(true);
+});
+
+it('render App-body', () => {
+  expect(wrapper.find('body.App-body').exists()).toEqual(true);
+});
+
+it('render App-footer', () => {
+  expect(wrapper.find('footer.App-footer').exists()).toEqual(true);
+});
