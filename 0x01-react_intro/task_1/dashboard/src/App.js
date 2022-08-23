1f199ff153e703faf06e3a9da440ca89228205f4
DaboRestrepo <daborestrepo@gmail.com>
2022-08-23 15:46:15 -0500

    Add some utils to the app

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils.js';
+
+export default function App() {
+  return (
+    <>
+    <header className="App-header">
+      <img src={logo} alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+    <body className="App-body">
+      <p>Login to access the full dashboard</p>
+    </body>
+    <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+    </footer>
+    </>
+  );
+}
