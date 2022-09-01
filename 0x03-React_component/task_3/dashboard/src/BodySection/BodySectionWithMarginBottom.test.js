56acde695c37c9d424ade5809177285813dcd835
DaboRestrepo <daborestrepo@gmail.com>
2022-09-01 18:36:08 -0500

    Add the tests

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
+import BodySection from "./BodySection";
+
+describe('<BodySectionWithMarginBottom />', () => {
+  it('Renders BodySection', () => {
+    const wrapper = shallow((
+      <BodySectionWithMarginBottom title="test title">
+        <p>test children node</p>
+      </BodySectionWithMarginBottom>
+    ));
+    expect(wrapper.find('BodySection').exists()).toEqual(true);
+    const BodySection = wrapper.find('BodySection');
+    const internalBody = BodySection.dive();
+    const h2 = internalBody.find('h2');
+    const p = internalBody.find('p');
+    expect(h2).toHaveLength(1);
+    expect(h2.text()).toEqual('test title');
+    expect(p).toHaveLength(1);
+    expect(p.text()).toEqual('test children node');
+  });
+})
