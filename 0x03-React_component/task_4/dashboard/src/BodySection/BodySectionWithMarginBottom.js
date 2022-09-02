2db5aef7078028b30537b17d462dd1fd6d64674c
DaboRestrepo <daborestrepo@gmail.com>
2022-09-01 20:39:53 -0500

    WithLogging

---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from '../BodySection/BodySection';
+import './BodySectionWithMarginBottom.css';
+
+export default class BodySectionWithMarginBottom extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+      <div className='bodySectionWithMargin'>
+        <BodySection {...this.props} />
+      </div>
+      </>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.defaultProps = {
+  title: ''
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string
+}
---
+++ 0x03-React_component/task_4/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import CourseList from "./CourseList";
+import CourseListRow from './CourseListRow';
+
+describe('<CourseList />', () => {
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
+  it('Renders withput crashing', () => {
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it('Renders 5 rows', () => {
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.find(CourseListRow)).toHaveLength(3);
+  });
+  it('Renders without array', () =>{
+    const wrapper = shallow(<CourseList listCourses={[]} />);
+    expect(wrapper.find(CourseListRow)).toHaveLength(3);
+  });
+  it('Renders it correctly', () => {
+    const wrapper = shallow(<CourseList listCourses={listCourses}/>);
+    expect(wrapper.find(CourseListRow).first().html()).toEqual('<tr><th colSpan="2">Available courses</th></tr>');
+    expect(wrapper.find(CourseListRow)).toHaveLength(5);
+  });
+});
