6e17f501e304b5826fa007d84082a0c942c6e10b
DaboRestrepo <daborestrepo@gmail.com>
2022-09-21 18:40:54 -0500

    Change the perspective

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -2,12 +2,6 @@ import Immutable from 'immutable';
 
 const { List } = Immutable;
 
-export function getListObject(array) {
-  List(array);
-}
+export const getListObject = (array) => List(array);
 
-export function addElementToList(list, element) {
-  const newList = getListObject(list);
-  newList.push(element);
-  return newList;
-}
+export const addElementToList = (list, element) => list.push(element);
