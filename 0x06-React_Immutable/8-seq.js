68102be87fea30fae6adb8ef3c2d142323b7e775
DaboRestrepo <daborestrepo@gmail.com>
2022-09-21 20:10:12 -0500

    Fix the sign

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -6,7 +6,7 @@ const upperCase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 export default function printBestStudents(object) {
   const bestScore = Seq(object)
-    .filter((std) => std.score < 70)
+    .filter((std) => std.score > 70)
     .map((std) => {
       const { firstName, lastName } = std;
       return {
