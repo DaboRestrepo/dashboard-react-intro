752ccaf1d97c27f6028b633e102620e5e501ee4c
DaboRestrepo <daborestrepo@gmail.com>
2022-09-21 20:01:13 -0500

    Use seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,23 @@
+import Immutable from 'immutable';
+
+const { Seq } = Immutable;
+
+const upperCase = (str) => {
+  const str1 = str.charAt(0).toUpperCase() + str.slice(1);
+  return str1;
+};
+
+export default function printBestStudents(object) {
+  const bestScore = Seq(object)
+    .filter((std) => std.score > 70)
+    .map((std) => {
+      const { firstName, lastName } = std;
+      return {
+        ...std,
+        firstName: upperCase(firstName),
+        lastName: upperCase(lastName),
+      };
+    });
+
+  bestScore.forEach((std) => console.log(std));
+}
