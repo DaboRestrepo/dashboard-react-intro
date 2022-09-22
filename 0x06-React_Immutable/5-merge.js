b77092aec24f33865e812d8079b839dbb33639b9
DaboRestrepo <daborestrepo@gmail.com>
2022-09-21 19:12:26 -0500

    Return variables

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,17 @@
-import Immutable, { concat, merge } from 'immutable';
+import Immutable from 'immutable';
 
 const { List, Map } = Immutable;
 
 export const concatElements = (page1, page2) => {
   const list1 = List(page1);
   const list2 = List(page2);
-  return concat(list1, list2);
+  const list3 = list1.concat(list2);
+  return list3;
 };
 
 export const mergeElements = (page1, page2) => {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  return merge(map1, map2);
+  const map3 = map1.merge(map2);
+  return map3;
 };
